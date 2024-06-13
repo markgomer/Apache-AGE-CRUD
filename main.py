95f1f88c4cf5577c76b4cad838b44c6c2a99e029
Marco A S <aureliojuniorcmrj@hotmail.com>
2024-06-13 11:11:28 -0300

    Small changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import psycopg2
 from DAO import DAO
 from Writer import Writer
 
@@ -7,22 +6,29 @@ from Writer import Writer
 GRAPH_NAME = "test_graph"
 
 # get database configuration
-config = "host=127.0.0.1 port=5455 dbname=postgresDB user=postgresUser password=postgresPW"
+config = (
+    "host=127.0.0.1 \
+    port=5455 \
+    dbname=postgresDB \
+    user=postgresUser \
+    password=postgresPW"
+)
 
 # create data access object
-Dao = DAO(GRAPH_NAME, config);
+Dao = DAO(GRAPH_NAME, config)
 
 # connect to the database
 connection = Dao.connect()
 
-# create writer object 
+# create writer object
 writer = Writer()
 
 # send test queries
 # "CREATE (n:Person {name: 'Joe'})"
-v_label = 'Person'
+v_label = "Person"
 v_prop = "{name: 'Joe'}"
 query = writer.create_vertex(v_label, v_prop)
 Dao.execQuery(query)
 
 Dao.close_connection()
+
